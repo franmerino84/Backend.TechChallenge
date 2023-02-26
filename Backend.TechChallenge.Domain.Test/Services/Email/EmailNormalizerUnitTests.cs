5878ac87d6de1068cd4021ebff17183d91a09ce0
Francisco Merino <franciscojavier.merino@voxsmart.com>
2023-02-26 16:43:17 +0100

    EmailNormalizer tests + changes

--- Backend.TechChallenge.Domain.Test/Services/Email/EmailNormalizerUnitTests.cs
+++ Backend.TechChallenge.Domain.Test/Services/Email/EmailNormalizerUnitTests.cs
@@ -1,7 +1,58 @@
-﻿namespace Backend.TechChallenge.Domain.Test.Services.Email
+﻿using Backend.TechChallenge.Domain.Services.Email;
+using Xunit;
+
+namespace Backend.TechChallenge.Domain.Test.Services.Email
 {
     public class EmailNormalizerUnitTests
     {
-        //TODO
+        private readonly EmailNormalizer _emailNormalizer;
+
+        public EmailNormalizerUnitTests()
+        {
+            _emailNormalizer = new EmailNormalizer();
+        }
+
+        [Fact]
+        public void Normalize_ValidEmail_SameEmail()
+        {
+            var validEmail = "test@email.com";
+
+            var result = _emailNormalizer.Normalize(validEmail);
+
+            Assert.Equal(validEmail, result);
+        }
+
+        [Fact]
+        public void Normalize_EmailWithPlus_RemovePlusAndFollowingCharactersBeforeAt()
+        {
+            var email = "test+other@email.com";
+            var expected = "test@email.com";
+
+            var result = _emailNormalizer.Normalize(email);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Normalize_EmailWithDot_RemoveDot()
+        {
+            var email = "test.other@email.com";
+            var expected = "testother@email.com";
+
+            var result = _emailNormalizer.Normalize(email);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Normalize_EmailWithDotAbndPlus_RemoveDotAndPlusAndFolllowingCharactersBeforeAt()
+        {
+            var email = "test.other+another@email.com";
+            var expected = "testother@email.com";
+
+            var result = _emailNormalizer.Normalize(email);
+
+            Assert.Equal(expected, result);
+        }
     }
 }
--- Backend.TechChallenge.Domain/Services/Email/EmailNormalizer.cs
+++ Backend.TechChallenge.Domain/Services/Email/EmailNormalizer.cs
@@ -2,13 +2,15 @@
 {
     public class EmailNormalizer : IEmailNormalizer
     {
+        //I haven't change the behavior of this method (just reordering remove and replace due to some unit tests failing),
+        // although I think it doesn't make a lot of sense...
         public string Normalize(string email)
         {
             var aux = email.Split('@', StringSplitOptions.RemoveEmptyEntries);
 
-            var atIndex = aux[0].IndexOf("+", StringComparison.Ordinal);
+            var plusIndex = aux[0].IndexOf("+", StringComparison.Ordinal);
 
-            aux[0] = atIndex < 0 ? aux[0].Replace(".", "") : aux[0].Replace(".", "").Remove(atIndex);
+            aux[0] = plusIndex < 0 ? aux[0].Replace(".", "") : aux[0].Remove(plusIndex).Replace(".", "");
 
             return $"{aux[0]}@{aux[1]}";            
         }
