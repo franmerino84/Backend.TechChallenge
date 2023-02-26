4c592b948b1058d3f79b4095a9a3bc974ba755bb
Francisco Merino <franciscojavier.merino@voxsmart.com>
2023-02-26 16:53:09 +0100

    More tests + reverted database move

--- Backend.TechChallenge.Persistence.File.Test/UnitOfWorkUnitTests.cs
+++ Backend.TechChallenge.Persistence.File.Test/UnitOfWorkUnitTests.cs
@@ -1,7 +1,41 @@
-﻿namespace Backend.TechChallenge.Persistence.File.Test
+﻿using Backend.TechChallenge.Domain.Contracts;
+using Moq;
+using System.Runtime.CompilerServices;
+using Xunit;
+
+namespace Backend.TechChallenge.Persistence.File.Test
 {
     public class UnitOfWorkUnitTests
     {
-        //TODO
+        private readonly Mock<IUsersRepository> _usersRepository;
+        private readonly UnitOfWork _unitOfWork;
+
+        public UnitOfWorkUnitTests()
+        {
+            _usersRepository = new Mock<IUsersRepository>();
+            _unitOfWork = new UnitOfWork(_usersRepository.Object);
+        }
+
+        [Fact]
+        public async Task SaveChanges_CallsUsersRepositorySaveChanges()
+        {
+            await _unitOfWork.SaveChanges();
+
+            _usersRepository.Verify(x=>x.SaveChanges());
+        }
+
+        [Fact]
+        public async Task Rollback_CallsUsersRepositoryRollback()
+        {
+            await _unitOfWork.Rollback();
+
+            _usersRepository.Verify(x => x.Rollback());
+        }
+
+        [Fact]
+        public void UsersRepository_ReturnInjectedOne()
+        {
+            Assert.Equal(_usersRepository.Object, _unitOfWork.UsersRepository);
+        }
     }
 }
