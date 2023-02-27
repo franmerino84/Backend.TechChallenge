4175b57486618d4e32aa68bbdc52c2d2961ca8ef
Francisco Merino <franciscojavier.merino@voxsmart.com>
2023-02-27 09:34:26 +0100

    More tests

--- Backend.TechChallenge.Application.Test/Mappings/UserTypeMappingProfileIntegrationTests.cs
+++ Backend.TechChallenge.Application.Test/Mappings/UserTypeMappingProfileIntegrationTests.cs
@@ -1,7 +1,26 @@
-﻿namespace Backend.TechChallenge.Application.Test.Mappings
+﻿using AutoMapper;
+using Backend.TechChallenge.Api.Dtos.Users.Post;
+using Backend.TechChallenge.Application.Users.Command.CreateUser;
+using Backend.TechChallenge.Common.Test;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Backend.TechChallenge.Application.Test.Mappings
 {
     public class UserTypeMappingProfileIntegrationTests
     {
-        //TODO
+        [Theory]
+        [InlineData(Application.Users.UserType.Normal, Domain.Entities.Users.UserType.Normal)]
+        [InlineData(Application.Users.UserType.SuperUser, Domain.Entities.Users.UserType.SuperUser)]
+        [InlineData(Application.Users.UserType.Premium, Domain.Entities.Users.UserType.Premium)]
+        public void ApplicationUserType_Map_ExpectedDomainUserType(Application.Users.UserType input, Domain.Entities.Users.UserType expected)
+        {
+            var mapper = ServiceProviderBuilder.Build().GetService<IMapper>();
+
+            var result = mapper?.Map<Domain.Entities.Users.UserType>(input);
+
+            Assert.Equal(expected, result);
+        }
     }
 }
--- Backend.TechChallenge.Persistence.File.Test/Mappings/FileUserMappingProfileIntegrationTests.cs
+++ Backend.TechChallenge.Persistence.File.Test/Mappings/FileUserMappingProfileIntegrationTests.cs
@@ -1,7 +1,46 @@
-﻿namespace Backend.TechChallenge.Persistence.File.Test.Mappings
+﻿using AutoMapper;
+using Backend.TechChallenge.Common.Test;
+using Backend.TechChallenge.Domain.Entities.Users;
+using Backend.TechChallenge.Persistence.File.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Backend.TechChallenge.Persistence.File.Test.Mappings
 {
     public class FileUserMappingProfileIntegrationTests
     {
-        //TODO
+        [Fact]
+        public void User_Map_FileUser()
+        {
+            User response = GetValidCreateUser();
+            FileUser expected = GetValidFileUser();
+
+            var mapper = ServiceProviderBuilder.Build().GetService<IMapper>();
+
+            var result = mapper?.Map<FileUser>(response);
+
+            var serializedExpected = JsonConvert.SerializeObject(expected);
+            var serializedResult = JsonConvert.SerializeObject(result);
+
+            Assert.Equal(serializedExpected, serializedResult);
+        }
+
+        private static User GetValidCreateUser() =>
+            new NormalUser("name", "email", "address", "456", 123);
+
+
+        private static FileUser GetValidFileUser()
+        {
+            return new FileUser
+            {
+                Address = "address",
+                Email = "email",
+                Money = 123,
+                Name = "name",
+                Phone = "456",
+                UserType = "Normal"
+            };
+        }
     }
 }
